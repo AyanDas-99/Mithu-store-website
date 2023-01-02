816241454c62a9d665eccdca54361ea43485af7c
Ayan Das <ayandas9531@gmail.com>
2023-01-02 17:10:27 +0530

    signing in user

--- src/pages/login/signin.tsx
+++ src/pages/login/signin.tsx
@@ -1,5 +1,26 @@
+import { provider, auth } from '../../config/firebase';
+import { signInWithPopup, signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
 export const Login = () => {
+
+    // navigator
+    const navigator = useNavigate();
+
+    const googleSignIn = async () => {
+        await signInWithPopup(auth, provider);
+        console.log(auth.currentUser)
+        navigator('/')
+    }
+
+    const eventSignOut = async () => {
+        await signOut(auth);
+    }
+
     return (
-        <div></div>
+        <div className="Login-form">
+            <button onClick={googleSignIn}>Sign In</button>
+            <button onClick={eventSignOut}>Sign Out</button>
+        </div>
     )
 }
